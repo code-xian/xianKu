6cded083316c12faccfc500c3fa6141c73f2a511
code-xian <code_xian@163.com>
2020-04-20 10:25:49 +0800

    websocket1

---
+++ jxc/src/test/java/zzx/jxc/JxcApplicationTests.java
@@ -0,0 +1,13 @@
+package zzx.jxc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JxcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
